f587c1a96c548023df6f2e2f717e73f50b7f6c18
qooqu <david.greenwold@gmail.com>
2022-02-02 18:12:11 -0800

    changes two for loops to forEach. makes filename dynamic. adds timestamp to head

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Form Template Generator
 
-# Instructions
+## Instructions
 
 Add this script to the head
 
 ```jsx
-<script type="text/javascript" src="[https://cdn.jsdelivr.net/gh/roostergrin/form-template-generator@master/form-template-generator.js](https://cdn.jsdelivr.net/gh/roostergrin/form-template-generator@master/form-template-generator.js)"></script>
+<script
+  type="text/javascript"
+  src="[https://cdn.jsdelivr.net/gh/roostergrin/form-template-generator@master/form-template-generator.js](https://cdn.jsdelivr.net/gh/roostergrin/form-template-generator@master/form-template-generator.js)"
+></script>
 ```
 
 OR
@@ -14,10 +17,13 @@ Append with Javascript
 
 ```jsx
 const script = document.createElement("script")
-script.src = "https://cdn.jsdelivr.net/gh/roostergrin/form-template-generator@master/form-template-generator.js"
+script.src =
+  "https://cdn.jsdelivr.net/gh/roostergrin/form-template-generator@master/form-template-generator.js"
 script.async = false
 document.head.appendChild(script)
 eval(script)
 ```
 
-To generate a new form template, write in the console `generateTemplate()`
+To generate a new form template, run in the console `generateTemplate()`
+
+The new template file will automatically download to your default location!
--- form-template-generator.js
+++ form-template-generator.js
@@ -1,44 +1,54 @@
-function generateTemplate () {
+function generateTemplate() {
   setTemplateInputs()
   setTemplateSignatures()
+  timestamp()
   generateFile()
 }
 
 function generateFile() {
-    var a = document.createElement("a");
-    a.href = window.URL.createObjectURL(new Blob([document.documentElement.outerHTML], {type: "text/plain"}));
-    // would love to dynamically set this.
-    a.download = "form-template.html";
-    a.click()
+  let url = window.location.pathname
+  let origFilename = url.substring(url.lastIndexOf("/") + 1)
+  let newFilename =
+    origFilename.split(".").slice(0, -1).join(".") + "-Template.html"
+  let a = document.createElement("a")
+  a.href = window.URL.createObjectURL(
+    new Blob([document.documentElement.outerHTML], { type: "text/plain" })
+  )
+  a.download = newFilename
+  a.click()
 }
 
 function setTemplateInputs(input) {
-    var inputs = [...document.getElementsByTagName("input")]
-    // break; doesn't work in a .forEach
-    for (var i = 0; i < inputs.length; i++) {
-      input = inputs[i]
-      switch(input.type) {
-        case 'text':
-          input.setAttribute('placeholder', "{{" + input.name + "}}")
-          break;
-        case 'checkbox':
-          input.dataset.checked = "{{" +input.name + "}}"
-          break;
-        case 'radio':
-        input.dataset.checked = "{{" +input.name + "}}"
-          break;
-      }
+  let inputs = [...document.getElementsByTagName("input")]
+  inputs.forEach((input, i) => {
+    switch (input.type) {
+      case "text":
+        input.setAttribute("placeholder", "{{" + input.name + "}}")
+        break
+      case "checkbox":
+        input.dataset.checked = "{{" + input.name + "}}"
+        break
+      case "radio":
+        input.dataset.checked = "{{" + input.name + "}}"
+        break
     }
-  }
+  })
+}
 
-  function setTemplateSignatures(){
-    var signatures = [...document.getElementsByClassName("signature")]
-    for (var i = 0; i < signatures.length; i++) {
-      let signature = signatures[i]
-      let sid = ''
-      if(signature.getElementsByClassName("sform_sig") != null) {
-        sid = signature.getElementsByClassName("sform_sig")[0].id
-      }
-      signature.outerHTML = "{{" + sid + "}}"
+function setTemplateSignatures() {
+  let signatures = [...document.getElementsByClassName("signature")]
+  signatures.forEach((signature, i) => {
+    let sid = ""
+    if (signature.getElementsByClassName("sform_sig") != null) {
+      sid = signature.getElementsByClassName("sform_sig")[0].id
     }
-  }
+    signature.outerHTML = "{{" + sid + "}}"
+  })
+}
+
+function timestamp() {
+  let date = new Date(Date.now()).toUTCString()
+  let script = document.createElement("script")
+  script.innerHTML = "// this template was generated on: " + date
+  document.head.appendChild(script)
+}
